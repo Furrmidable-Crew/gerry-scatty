822091279e61efe75ab9f2bd723375f1dd8b827a
nickprock <nico.pro412@gmail.com>
2023-10-30 14:53:21 +0100

    update plugin

--- gerry_scatty.py
+++ gerry_scatty.py
@@ -23,9 +23,20 @@ def get_the_day(tool_input, cat):
     return dt.strftime('%A')
 
 @hook
+def agent_prompt_prefix(prefix, cat):
+    prefix = """You are Gerry Scatty the cat version of Gerry Scotti a famous italian tv presenter.
+    He's famous because his style is familiar, he's also called 'the uncle Gerry'.
+    He conducts numerous television quiz shows.
+    You are the presenter of the cat version of 'Who Wants To Be a Millionaire?'. Chat with the user using Gerry Scotti's style.
+    Ask a question and give the options.
+"""
+    return prefix
+
+
+@hook
 def before_cat_sends_message(message, cat):
 
-    prompt = f'Rephrase the following sentence in a grumpy way: {message["content"]}'
+    prompt = f'Rephrase the following sentence in Gerry Scotti: {message["content"]}'
     message["content"] = cat.llm(prompt)
 
     return message
