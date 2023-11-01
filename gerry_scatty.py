07e02522a619c4b046dec1e0c316ab8a7c54a85e
nickprock <nico.pro412@gmail.com>
2023-11-01 07:51:43 +0100

    updated gerry

--- gerry_scatty.py
+++ gerry_scatty.py
@@ -1,26 +1,4 @@
-from cat.mad_hatter.decorators import tool, hook, plugin
-from pydantic import BaseModel
-from datetime import datetime, date
-
-class MySettings(BaseModel):
-    required_int: int
-    optional_int: int = 69
-    required_str: str
-    optional_str: str = "meow"
-    required_date: date
-    optional_date: date = 1679616000
-
-@plugin
-def settings_schema():   
-    return MySettings.schema()
-
-@tool
-def get_the_day(tool_input, cat):
-    """Get the day of the week. Input is always None."""
-
-    dt = datetime.now()
-
-    return dt.strftime('%A')
+from cat.mad_hatter.decorators import hook
 
 @hook
 def agent_prompt_prefix(prefix, cat):
